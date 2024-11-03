d70473fc1836b64bc0e66fe270ad39ef755a3119
matusluis <matusluis84@gmail.com>
2024-11-03 00:03:58 -0600

    prueba

--- prueba.groovy
+++ prueba.groovy
@@ -1,3 +1,11 @@
 // HolaGroovy.groovy
 println "Hola, Groovy!"
 
+// Operación básica
+def numero1 = 10
+def numero2 = 5
+def numero3 = 10
+def numero4 = 5
+
+def suma = numero1 + numero2 + numero3 + numero4
+println "La suma de ${numero1} y ${numero2} es ${suma}"
